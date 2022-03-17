b57056a992dc8b88b40b795a9afe9d7a7f0ed9c7
Timur Garipov <tr.garipov@yandex.ru>
2022-03-17 17:27:54 +0300

    agoToText

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,24 @@
 fun main() {
-
+    val x = agoToText(3500)
+    print(x)
 }
-fun agoToText(time:Int) {
-    when (time) {
-        0 - 60 -> "был(а) только что"
-        61 - 60*60 -> "был(а) $time ${agoInMinuteToText(time)} назад"
-        60*60+1 - 24*60*60 -> "был(а) ${time/60.toInt()} ${agoInHoursToText()} назад"
-        24*60*60 - 48*60*60 -> "был(а) сегодня"
-        48*60*60 - 24*3*60*60 -> "был(а) вчера"
+fun agoToText(time:Int): String {
+    return when  {
+        time < 61 -> "был(а) только что"
+        time < 60*60 -> "был(а) $time ${agoInMinuteToText(time)} назад"
+        time < 24*60*60 -> "был(а) ${time/60.toInt()} ${agoInHoursToText()} назад"
+        time < 48*60*60 -> "был(а) сегодня"
+        time < 24*3*60*60 -> "был(а) вчера"
         else -> "был(а) давно"
     }
 }
 fun agoInMinuteToText(time: Int) {
-
+    when {
+        time == 11 * 60 -> "минут"
+        time * 60 % 10 == 1 -> "минуту"
+        time * 60 % 10 == 2 || time * 60 % 10 == 3 || time * 60 % 10 == 4 -> "минуты"
+        else -> "минут"
+    }
 }
 
 fun agoInHoursToText() {
