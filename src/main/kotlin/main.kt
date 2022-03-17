342f7b65b0cfb1fba9632b0cd536634a3727d47d
Timur Garipov <tr.garipov@yandex.ru>
2022-03-17 17:41:47 +0300

    allFuns

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,27 @@
 fun main() {
-    val x = agoToText(3500)
-    print(x)
+    println(agoToText(60))
+    println(agoToText(3500))
+    println(agoToText(3700))
+    println(agoToText(7200))
+    println(agoToText(86300))
+    println(agoToText(172700))
+    println(agoToText(259100))
+
 }
-fun agoToText(time:Int): String {
-    return when  {
+
+fun agoToText(time: Int): String {
+    return when {
         time < 61 -> "был(а) только что"
-        time < 60*60 -> "был(а) $time ${agoInMinuteToText(time)} назад"
-        time < 24*60*60 -> "был(а) ${time/60.toInt()} ${agoInHoursToText()} назад"
-        time < 48*60*60 -> "был(а) сегодня"
-        time < 24*3*60*60 -> "был(а) вчера"
+        time < 60 * 60 -> "был(а) ${time / 60} ${agoInMinuteToText(time)} назад"
+        time < 24 * 60 * 60 -> "был(а) ${time / 60 / 60.toInt()} ${agoInHoursToText(time)} назад"
+        time < 48 * 60 * 60 -> "был(а) сегодня"
+        time < 24 * 3 * 60 * 60 -> "был(а) вчера"
         else -> "был(а) давно"
     }
 }
-fun agoInMinuteToText(time: Int) {
-    when {
+
+fun agoInMinuteToText(time: Int): String {
+    return when {
         time == 11 * 60 -> "минут"
         time * 60 % 10 == 1 -> "минуту"
         time * 60 % 10 == 2 || time * 60 % 10 == 3 || time * 60 % 10 == 4 -> "минуты"
@@ -21,6 +29,12 @@ fun agoInMinuteToText(time: Int) {
     }
 }
 
-fun agoInHoursToText() {
+fun agoInHoursToText(time: Int): String {
+    return when {
+        time == 60 * 60 * 11 -> "час"
+        time / 60 / 60 % 10 == 1 -> "час"
+        time / 60 / 60 % 10 == 2 || time / 60 / 60 % 10 == 3 || time / 60 / 60 % 10 == 4 -> "часа"
+        else -> "часов"
 
+    }
 }
